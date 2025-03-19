f6d94deab74743d534b4ce2b667a4eb98ca90a4b
Asli Tarakcilar <aslinur.tarakcilar.dev@gmail.com>
2025-03-19 12:22:30 +0300

    personel_muhasebe.py eklendi

---
+++ personel_muhasabe.py
@@ -0,0 +1,10 @@
+import os
+import datetime
+import random
+
+class PersonelMuhasebeModulu:
+    def __init__(self):
+        self.veri_klasoru = "veri"
+        self.personel_dosya = f"{self.veri_klasoru}/personel.txt"
+        self.nobet_dosya = f"{self.veri_klasoru}/nobet_cizelgesi.txt"
+        self.muhasebe_dosya = f"{self.veri_klasoru}/mu
